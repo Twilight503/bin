360e1e45568e48eb3b997e81a489a6d1e49de220
Marius <mariusrotaru.503@gmail.com>
2025-05-01 01:20:02 +0300

    update

--- update.py
+++ update.py
@@ -1,53 +1,19 @@
-import requests
-import shutil
+import gdown
 import zipfile
 import os
 
-def download_file_from_google_drive(file_id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-    response = session.get(URL, params={'id': file_id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': file_id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-    return True
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-def save_response_content(response, destination, chunk_size=32768):
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(chunk_size):
-            if chunk:
-                f.write(chunk)
-
-# ==== USAGE ====
-
 file_id = "1ykvdJv9t5w6ct-ndPEteXkNre2pCZ167"
-zip_destination = "update.zip"
+destination = "update.zip"
+url = f"https://drive.google.com/uc?id={file_id}"
 
-updated = download_file_from_google_drive(file_id, zip_destination)
+gdown.download(url, destination, quiet=False)
 
-if updated:
-    try:
-        shutil.rmtree('bin-main')
-    except:
-        pass
-    with zipfile.ZipFile("./update.zip", 'r') as zip_ref:
-        zip_ref.extractall("./")
-    try:
-        os.remove("./update.zip")
-    except:
-        pass
+with zipfile.ZipFile("./update.zip", 'r') as zip_ref:
+    zip_ref.extractall("./")
+try:
+    os.remove("./update.zip")
+except:
+    pass
 
 file_path = os.path.realpath(__file__)
 os.remove(file_path)
